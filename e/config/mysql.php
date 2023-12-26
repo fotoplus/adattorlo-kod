e5d12adb883dc0370329c2328b36d2f7a72222dd
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2023-12-26 20:07:13 +0100

    Update mysql.php

--- e/config/mysql.php
+++ e/config/mysql.php
@@ -4,7 +4,7 @@
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 // A kapcsolat karakterkódolása (megegyezik az adatbáziséval)
-$db_charset = 'utf8mb4_unicode_ci';
+$db_charset = 'utf8mb4';
 
 // Adatbázis tábláinak előtagja (ha van ilyen)
 $tbl_prefix = 'fp_';
