3dcf1b292bf9b418bd096a8f81257fb5682a007f
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2024-06-17 12:14:41 +0200

    Update receipt_number_validation.js
    A reguláris kifelyezések javítása

--- scripts/receipt_number_validation.js
+++ scripts/receipt_number_validation.js
@@ -6,7 +6,7 @@ function validateReceiptNumber() {
     if (value.includes('ORD') || value.includes('RET')) {
         validationDiv.textContent = 'Ez egy rendelésszám, a bizonylat számát add meg!';
         validationDiv.className = 'validation validation-error';
-    } else if (value.startsWith('A') && !value.includes('/')) {
+	} else if (/^[A]\d{8,}$/.test(value)) {
         validationDiv.textContent = 'Ez (eddig csak) az adóügyi nyomtató száma';
         validationDiv.className = 'validation validation-uncertain';
     } else if (value.startsWith('M/')) {
@@ -18,16 +18,16 @@ function validateReceiptNumber() {
     } else if (value.match(/^\d+\/\d+$/)) {
         validationDiv.textContent = 'Rendben, ez a nyugta száma';
         validationDiv.className = 'validation validation-success';
-	} else if (/^[A-Za-z0-9]{4,6}$/.test(value)) {
+	} else if (/^[A-Za-z0-9]{5}$/.test(value)) {
  		validationDiv.textContent = 'Nem jó, ez egy NAV ellenőrző kód, a nyugta vagy a számla számát add meg!';
 		validationDiv.className = 'validation validation-error';
     } else if (value.startsWith('A') && (value.match(/\//g) || []).length >= 2) {
         validationDiv.textContent = 'Rendben, ez egy nyugta száma';
         validationDiv.className = 'validation validation-success';
-	} else if (value.match(/^E?-FP[A-Z]{0,2}-\d{2}-\d{4}-\d+$/) || value.match(/^FP[A-Z]{0,2}-\d{4}-\d+$/)) {
+	} else if (value.match(/^(E-)?FP[A-Z]{0,2}-\d{2,4}-\d+$/)) {
 		validationDiv.textContent = 'Rendben, ez egy számlaszám';
 		validationDiv.className = 'validation validation-success';
-	}else {
+	} else {
         validationDiv.textContent = 'A mező értékét (még) nem ismertük fel';
         validationDiv.className = 'validation validation-uncertain';
     }
